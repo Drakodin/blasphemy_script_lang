321894fced831407591524d753603674503cb108
Drakodin <tony.y.tian@gmail.com>
2020-05-06 13:28:18 -0400

    Remove extra code, add functions

--- lang/lex.py
+++ lang/lex.py
@@ -13,14 +13,14 @@ class Lex(dict):
         self["open"] = "public"
         self["singular"] = "static"
         self["noret"] = "void"
-        self["core"] = "main(String[] args) { \n \t"
+        self["core"] = "main(String[] args)"
+        self["startfun"] = "{ \n \t"
         self["<>"] = "\n}"
         self["output"] = "System.out.println"
         # Code Architecture
         self["#"] = ""
         self["["] = "("
         self["]"] = ")"
-        self["stopcode"] = "\n}"
 
     # Method to add to the language during operation, if preferred
     def add_to_lang(self, k, v):
