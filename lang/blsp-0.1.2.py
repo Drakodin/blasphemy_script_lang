14e24bde2913b304ae1543c871df872a11f060bd
Drakodin <tony.y.tian@gmail.com>
2020-05-08 22:27:25 -0400

    Fix file processing by absolute path.

--- lang/blsp-0.1.2.py
+++ lang/blsp-0.1.2.py
@@ -4,18 +4,22 @@ from subprocess import Popen;
 from subprocess import PIPE
 
 def main(argv):
+    # Temporarily commented out due to odd errors
     # Generation of .java file and subsequent name of
-    target_file = inter.gen_name(argv[1])
+    true_path = inter.process_in(argv[1])
+    target_file = inter.gen_name(true_path)
+
+    # Internal call to the interpreter
     inter.parse_file(argv[1])
 
-
-    # Batch operation
+    # Batch operation for compilation
     if (argv[2] == "testcompile"):
         p = Popen(['blsp_run.bat', target_file], stderr=PIPE)
         out, err = p.communicate()
     elif (argv[2] == "export"):
         return 2
     return 0
+    
 
 if __name__ == "__main__":
     main(sys.argv)
