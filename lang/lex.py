c65245c0c8c8a8f2e8561ec9701d5af252235689
Drakodin <tony.y.tian@gmail.com>
2020-05-10 15:12:30 -0400

    Add initial 0.1.3 language syntax.

--- lang/lex.py
+++ lang/lex.py
@@ -9,18 +9,22 @@ class Lex(dict):
         # Datatypes
         self["num"] = "int"
         self["word"] = "String"
+        self["dec"] = "double"
         # Keywords and functions
         self["open"] = "public"
         self["singular"] = "static"
         self["noret"] = "void"
         self["core"] = "main(String[] args)"
-        self["startfun"] = "{ \n \t"
-        self["<>"] = "\n}"
+        self["start"] = "{ \n \t"
+        self["end"] = "\n}"
         self["output"] = "System.out.println"
         # Code Architecture
         self["#"] = ""
         self["["] = "("
         self["]"] = ")"
+        # Version .3 Architecture
+        self["array_build"] = "[]"
+        self["->"] = "."
 
     # Method to add to the language during operation, if preferred
     def add_to_lang(self, k, v):
