0b5d4f3ca0ae7c4aeaaecef032e3e68f4487a29a
Drakodin <tony.y.tian@gmail.com>
2020-05-11 17:04:56 -0400

    Rework blsp entry point, remove useless files.

--- blsp.bat
+++ blsp.bat
@@ -2,6 +2,7 @@
 title blsp
 
 if "%1" == "--version" goto :version
+if "%1" == "--v" goto :version
 if "%1" == "--help" goto :help
 
 if "%1" == "" (
@@ -33,23 +34,59 @@ goto :redirect
 
 :: Routing for batch operations
 :redirect
-if "%2" == "testcompile" goto :compile
+if "%2" == "debug" goto :compile
 if "%2" == "export" goto :export
+if "%2" == "run" goto :compile
 
 :: Script call to run JVM argument.
 :compile
-python %blsp_path% %d% testcompile
+python %blsp_path% %d%
+goto :jvm-setup
+
+:: Sets up file path for jvm operation
+:jvm-setup
+set "JAVA_EXT=.java"
+set "fn=%~n1"
+
+set "c1=%fn:~0,1%"
+for %%s in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do call set "c1=%%c1:%%s=%%s%%"
+set "fn=%c1%%fn:~1%"
+
+set "jp=%fn%%JAVA_EXT%"
+
+goto :jvm-call
+
+:: Calls the jvm attached to Path, whatever one comes first.
+:jvm-call
+javac %jp%
+echo.
+rem Something about this line without quotes causes a "Bad Magic Number" error
+rem Therefore, quotes were added to avoid it. Reason: "CAFEBABE" missing.
+echo File compiled: "%fn%.java -> %fn%.class"
+echo.
+ping 127.0.0.1 -n 2 > nul
+
+::Routing to essentially remove .java file after compilation.
+if "%2" == "run" goto :run
+
+echo Executing...
+
+java %fn%
 goto :result
 
+:run
+del /f %fn%.java
+echo Executing...
+java %fn%
+goto :result
 
 :: Only exports .java file, does not check compilation.
 :export
-python %blsp_path% %d% export
+python %blsp_path% %d%
+echo File generated: %1 -> %fn%.java
 goto :result
 
 :result
-echo.
-echo Java file generated from %1
 echo.
 echo Task Executed.
 goto :end
@@ -57,20 +94,22 @@ goto :end
 
 
 :version
-echo Blasphemy Script Interpreter v0.1.2.1
-echo Blasphemy Script Language v0.1.3
+echo Blasphemy Script Interpreter v0.1.4
+echo Blasphemy Script Language v0.1.4
 goto :end
 
 :help
 set "TAB=   "
 
 echo List of possible arguments
-echo %TAB% ["--version"]              : gets current version of Blasphemy Script
-echo %TAB% ["--help"]                 : gets a list of possible commands
-echo %TAB% ["<file.blsp> export"]     : transcribes input into a Java file
-echo %TAB%                              containing a class of the same name
-echo %TAB% ["<file.blsp> testcompile"]: exports transcribed Java file
-echo %TAB%                              and compiles it using JAVA_HOME
+echo %TAB% ["--version", "-v"]              : Gets current version of Blasphemy Script
+echo %TAB% ["--help"]                       : Gets a list of possible commands
+echo %TAB% ["<file.blsp> export"]           : Transcribes input into a Java file
+echo %TAB%                                    containing a class of the same name
+echo %TAB% ["<file.blsp> debug"]            : Exports transcribed Java file
+echo %TAB%                                    and compiles it with JVM on Path
+echo %TAB% ["<file.blsp> run"]              : Exports Java file to be compiled and run. 
+echo %TAB%                                    Removes transcribed file after compilation.
 
 goto :end
 
--- lang/blsp-0.1.2.py
+++ lang/blsp-0.1.2.py
@@ -4,20 +4,8 @@ from subprocess import Popen;
 from subprocess import PIPE
 
 def main(argv):
-    # Temporarily commented out due to odd errors
-    # Generation of .java file and subsequent name of
-    true_path = inter.process_in(argv[1])
-    target_file = inter.gen_name(true_path)
-
     # Internal call to the interpreter
     inter.parse_file(argv[1])
-
-    # Batch operation for compilation
-    if (argv[2] == "testcompile"):
-        p = Popen(['blsp_run.bat', target_file], stderr=PIPE)
-        out, err = p.communicate()
-    elif (argv[2] == "export"):
-        return 2
     return 0
     
 
