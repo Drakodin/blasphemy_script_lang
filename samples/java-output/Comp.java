0224b16d9d5aaae5c961e9fc985ea61e575f7a22
Tony Tian <57043533+Drakodin@users.noreply.github.com>
2020-05-11 18:13:10 -0400

    Add transcribed Java export file for comp.blsp
    Contains the generated source code.

---
+++ samples/java-output/Comp.java
@@ -0,0 +1,9 @@
+public class Comp {
+// Funfact, comments work the same due to printing of unknown tokens
+ private static java.util.ArrayList fun(int [] input){ 
+ 	 java.util.ArrayList <Integer>list=new java.util.ArrayList <Integer>();for(int i=0;i<input. length;i++){ 
+ 	 list. add(input[ i] );} return list;} public static void print_fun(){ 
+ 	 int [] numbers={ 
+ 	 0,2,4,6,8,10} ;System.out.println("List: "+ fun(numbers));} public static void main(String[] args) { 
+ 	 print_fun();} 
+} 
